3d17670638a63bcb0970b49d79b9711304c12bcf
khaleda nahar <khaledanahar43@gmail.com>
2022-01-17 17:42:04 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+import './App.scss';
+// import Header from 'component/Header'
+// import Banner from 'component/Banner'
+// import LogoSlider from 'component/Logo-slider'
+// import Card from 'component/Card'
+// import FooterTop from 'component/FooterTop'
+// import Footer from 'component/Footer'
+
+function App() {
+  return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    <>
+      {/* <Header/>
+      <Banner/>
+      <LogoSlider/>
+      <Card/>
+      <FooterTop/>
+      <Footer/> */}
+      <h1>lorem ispum</h1>
+    </>
+  );
+}
+
+export default App;
